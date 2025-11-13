cafbccb45359435ef44c8f2e08edced67b514a76
thomil13 <thomas.milde13@gmail.com>
2025-11-13 12:54:20 +0000

    test on local server

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path('blog/', blog_views.my_blog, name='blog'),
 
 ]
