39ad8e1e0a7481e1b0ec75ebaac19580615a0cae
thomil13 <thomas.milde13@gmail.com>
2025-11-14 16:33:00 +0000

    Add Excerpt field to Post model

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    excerpt = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
