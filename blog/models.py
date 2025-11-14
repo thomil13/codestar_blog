dd97616b431c0ae65d2d813c8e7ac2f8a04cb2e0
thomil13 <thomas.milde13@gmail.com>
2025-11-14 16:43:09 +0000

    Add field Updated_on to post

--- blog/models.py
+++ blog/models.py
@@ -11,4 +11,5 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
